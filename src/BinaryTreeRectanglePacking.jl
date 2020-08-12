cbf77f8eeb689ede47841f3fad77de55606ba326
Oliver Evans <oliverevans96@gmail.com>
2020-08-12 13:55:14 -0400

    Use args in run func

--- src/BinaryTreeRectanglePacking.jl
+++ src/BinaryTreeRectanglePacking.jl
@@ -8,19 +8,15 @@ include("Structs.jl")
 include("Packing.jl")
 include("Plotting.jl")
 
-function run()
-	W = 5
-	H = 4
-	N = 200
-
+function run(W::Real, H::Real, num_shapes::Integer, num_rectangles::Integer; plot=true, logging=true)
 	container = Structs.Rectangle{Int}(missing, 0, 0, W, H)
 
 	# Generate some random shapes
-	shapes = [rand(2) for i=1:10]
+	shapes = [rand(2) for i=1:num_shapes]
 	# Make many copies of them
-	wh = rand(shapes, N)
+	wh = rand(shapes, num_rectangles)
 	# Create rectangle sizes
-	rect_sizes = [Structs.RectangleSize(i, wh[i]...) for i=1:N]
+	rect_sizes = [Structs.RectangleSize(i, w, h) for (i, (w, h)) in enumerate(wh)]
 	# Sort them first
 	sorted_rect_sizes = sort(rect_sizes; by=Structs.max_side, rev=true)
 
@@ -32,10 +28,14 @@ function run()
 	num_packed = packed_nodes |> length
 	efficiency = sum(packed_nodes .|> Structs.area) / Structs.area(root)
 
-	println("Successfully packed $(num_packed)/$N rectangles")
-	println("Percent of available space used: $(100*round(efficiency, digits=2))%")
+	if logging
+		println("Successfully packed $(num_packed)/$num_rectangles rectangles")
+		println("Percent of available space used: $(100*round(efficiency, digits=2))%")
+	end
 
-	Plotting.plot(root)
+	if plot
+		Plotting.plot(root)
+	end
 end
 
 end # module
